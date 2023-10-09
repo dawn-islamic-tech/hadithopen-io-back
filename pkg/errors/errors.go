3904562b36bb2fc258fa8366485d2d3c707e81de
Усман Байбиков <u.baibikov@corp.mail.ru>
2023-10-09 07:23:31 +0400

    feat(error): upd error pkg funcitons

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -9,16 +9,30 @@ import (
 )
 
 type Error struct {
-	err error
+	msg string
 }
 
-func (e *Error) Error() string { return e.err.Error() }
+func New(msg string) error { return &Error{msg: msg} }
+
+func Is(err, target error) bool { return errors.Is(err, target) }
+
+func (e *Error) Error() string { return e.msg }
 
 func Unwrap(err error) error { return errors.Unwrap(err) }
 
 func Errorf(format string, args ...any) error { return fmt.Errorf(format, args...) }
 
 func Join(err ...error) error { return errors.Join(err...) }
+
+func JoinCloser(err error, closer ...io.Closer) error {
+	errs := make([]error, 0, len(closer)+1)
+	errs = append(errs, err)
+	for _, c := range closer {
+		errs = append(errs, c.Close())
+	}
+
+	return Join(errs...)
+}
 
 func AppendCloser(into *error, closer io.Closer) { multierr.AppendInto(into, closer.Close()) }
 
